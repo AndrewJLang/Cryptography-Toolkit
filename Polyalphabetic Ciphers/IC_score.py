53a08dcc2c489f68a2f66cba7d07e923f64a6444
Andrew <andrewespn@hotmail.com>
2019-10-16 22:36:20 -0700

    added multiplication table for input value in decipher.py

--- Polyalphabetic Ciphers/IC_score.py
+++ Polyalphabetic Ciphers/IC_score.py
@@ -1,7 +1,7 @@
 import numpy as np
 import math
 
-message = "YBR GPT OOY CBC GUG SNR TCW MVF RMU GJC MUI RCC UZV LJX BAJ DNU RTJ LLF KFF YBL JMZ NWG YNY JYB RRV HCG VLL MGH DKB NCN JHF NRW YNY JDV VTL ZGO RPX IAR QBY PNL YYI RLG YTV LYI GUG SEN OMZ NVA YSS IRP DXR SGS CGR UFS BHP GLN VLX BNI CJP BYT JXG BEJ NHF MZN BSR MYE NGS ZVA BBB REC YBR OCW LVR QFL RNL IER RNZ MSE MRA RGR NHT XGQ FRQ MZL OEY NHF QGI HBG CAI YIC YIU RJU OFT PFM CEC FFY LJF LTR LZG ORP XIE GMQ IBX YYN UVL LMV AYM OAQ PJX GUM ZMN ABI CZR LXC BAQ"
+message = "OTG WRV RHU GXO XUQ UEK VYO TSE NYU NSI YSH MHB BUR ZTR CCD DAK MHC ALI OJZ UOT JYC KFN KIO THO GWK Z"
 
 message = message.replace(" ", "")
 
@@ -40,6 +40,6 @@ def ICScore(my_list, listLength):
     sumNums = sum(squaredNums)
     return (sumNums / (messageLength**2))
 
-# print(f"Letter frequencies: {countLetterFrequencyNoArg()}")
+print(f"Letter frequencies: {countLetterFrequencyNoArg()}")
 
-# print(f"IC Score: {ICScore(countLetterFrequencyNoArg(), messageLength)}")
+print(f"IC Score: {ICScore(countLetterFrequencyNoArg(), messageLength)}")
