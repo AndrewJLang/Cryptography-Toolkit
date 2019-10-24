e596096a966a7b6eb44fe05a47dd8c47de8ab008
Andrew <andrewespn@hotmail.com>
2019-10-23 20:06:35 -0700

    minor changes to multiple files

--- Decrypting Monoalphabetic Ciphers/decrypt_affine.py
+++ Decrypting Monoalphabetic Ciphers/decrypt_affine.py
@@ -1,7 +1,8 @@
 import numpy as np
 import ascii_conversion
 
-message = "YBR GPT OOY CBC GUG SNR TCW MVF RMU GJC MUI RCC UZV LJX BAJ DNU RTJ LLF KFF YBL JMZ NWG YNY JYB RRV HCG VLL MGH DKB NCN JHF NRW YNY JDV VTL ZGO RPX IAR QBY PNL YYI RLG YTV LYI GUG SEN OMZ NVA YSS IRP DXR SGS CGR UFS BHP GLN VLX BNI CJP BYT JXG BEJ NHF MZN BSR MYE NGS ZVA BBB REC YBR OCW LVR QFL RNL IER RNZ MSE MRA RGR NHT XGQ FRQ MZL OEY NHF QGI HBG CAI YIC YIU RJU OFT PFM CEC FFY LJF LTR LZG ORP XIE GMQ IBX YYN UVL LMV AYM OAQ PJX GUM ZMN ABI CZR LXC BAQ"
+# message = "YBR GPT OOY CBC GUG SNR TCW MVF RMU GJC MUI RCC UZV LJX BAJ DNU RTJ LLF KFF YBL JMZ NWG YNY JYB RRV HCG VLL MGH DKB NCN JHF NRW YNY JDV VTL ZGO RPX IAR QBY PNL YYI RLG YTV LYI GUG SEN OMZ NVA YSS IRP DXR SGS CGR UFS BHP GLN VLX BNI CJP BYT JXG BEJ NHF MZN BSR MYE NGS ZVA BBB REC YBR OCW LVR QFL RNL IER RNZ MSE MRA RGR NHT XGQ FRQ MZL OEY NHF QGI HBG CAI YIC YIU RJU OFT PFM CEC FFY LJF LTR LZG ORP XIE GMQ IBX YYN UVL LMV AYM OAQ PJX GUM ZMN ABI CZR LXC BAQ"
+message = "ESP PYR TYP PCE STY VDZ QST DPB FLE TZY DLD LYL AAC ZIT XLE TZY EZC PLW TEJ ESP ASJ DTN TDE EST YVD CPL WTE JTD LYL AAC ZIT XLE TZY EZS TDP BFL ETZ YDE SPX LES PXL ETN TLY OZP DYE NLC P"
 
 message = ascii_conversion.stripSpaces(message)
 
@@ -15,17 +16,15 @@ def affineFunc(aInv,b,val):
 def decryptionCalc(aInv,b,val):
     return ((val-b)*aInv) % 26
 
-# def decryptedMessage(text):
-#     decrypted = []
-#     for x in range(len(text)):
-#         # decrypted.append(affineFunc(3,21, text[x])-3)
-#         decrypted.append(affineFunc(3,7,text[x]))
-#     return ascii_conversion.convertToText(decrypted)
+def properDecryption(aInv, b, val):
+    return ((val-b) % 26)
 
 def decryptedMessage(text):
     decrypted = []
     for x in range(len(text)):
-        decrypted.append(decryptionCalc(3,7,text[x]))
+        # decrypted.append(decryptionCalc(1,11,text[x]))
+        # decrypted.append(affineFunc(1,11,text[x]))
+        decrypted.append(properDecryption(1,11,text[x]))
     return ascii_conversion.convertToText(decrypted)
 
 print(decryptedMessage(message))
