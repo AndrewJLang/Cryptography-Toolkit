bcc306d56c90a24f56aa74b2dc4e5f9ff7d82cb8
Andrew <andrewespn@hotmail.com>
2019-11-02 16:16:40 -0700

    switched directories

--- LFSR/check_primitive.py
+++ LFSR/check_primitive.py
@@ -5,11 +5,11 @@ potentialPrimitives = [[1,1,1,1,1], [1,1,0,0,1],[1,0,1,0,1],[1,0,0,1,1]]
 
 degree = 4
 
-zeroList = np.zeros(15)
-print(zeroList)
+
 
 def getBaseEquations(deg):
     count = deg**2-1
     divisorList = []
     # for x in range(count):
         # np.zeros
+        np.zeros(15)
